5766422ed87851851314ea692e115b321f411d1c
Mahroof <maharoof@iocod.com>
2026-06-26 15:05:19 +0530

    feat: make AppLogo responsive to brand images and hide text when custom logos are provided

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -7,16 +7,19 @@ const brand = computed(() => usePage().props.brand);
 </script>
 
 <template>
-    <div class="flex aspect-square size-8 items-center justify-center">
+    <div
+        class="flex items-center justify-center"
+        :class="brand?.logo ? 'h-8 w-auto' : 'aspect-square size-8'"
+    >
         <img
             v-if="brand?.logo"
             :src="brand.logo"
             :alt="brand.name"
-            class="size-8 object-contain"
+            class="h-8 w-auto max-w-[140px] object-contain"
         />
         <AppLogoIcon v-else class="size-8" />
     </div>
-    <div class="ml-2 grid flex-1 text-left text-sm">
+    <div class="ml-2 grid flex-1 text-left text-sm" v-if="!brand?.logo">
         <span
             class="mb-0.5 truncate leading-tight font-semibold text-neutral-900 dark:text-neutral-100"
             >{{ brand?.name ?? 'Build Tech' }}</span
